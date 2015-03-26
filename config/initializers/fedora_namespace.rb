44fd37cd2eb3100ae6129390ab6906d3e5dd66fc
Chris Colvard <cjcolvar@indiana.edu>
2015-03-26 04:12:33 -0400

    Handle exceptions [skip ci]

--- config/initializers/fedora_namespace.rb
+++ config/initializers/fedora_namespace.rb
@@ -1,3 +1,8 @@
-ActiveFedora.fedora.base_path << "/#{Avalon::Configuration.lookup('fedora.namespace')}"
-ActiveFedora.fedora.root_resource_path << "/#{Avalon::Configuration.lookup('fedora.namespace')}"
-ActiveFedora.fedora.init_base_path
+begin
+  ActiveFedora.fedora.base_path << "/#{Avalon::Configuration.lookup('fedora.namespace')}"
+  ActiveFedora.fedora.root_resource_path << "/#{Avalon::Configuration.lookup('fedora.namespace')}"
+  ActiveFedora.fedora.init_base_path
+rescue Exception => e
+  puts e.message
+  puts e.backtrace.join("\n")
+end
